694123bbc33c9d65ba8ae1413851013ddc7cb826
Laurent Thiebault <thiebault.laurent@gmail.com>
2024-12-06 09:31:22 +0100

    refactor: some tests

--- day_1/test_day_1_1.py
+++ day_1/test_day_1_1.py
@@ -4,11 +4,11 @@ from main_day_1_1 import calculate_total_distance
 def test_calculate_total_distance(tmp_path):
     input_file = tmp_path / "input.txt"
     input_data = """3   4
-4   3
-2   5
-1   3
-3   9
-3   3"""
+                    4   3
+                    2   5
+                    1   3
+                    3   9
+                    3   3"""
     input_file.write_text(input_data)
 
     result = calculate_total_distance(str(input_file))
--- day_1/test_day_1_2.py
+++ day_1/test_day_1_2.py
@@ -4,11 +4,11 @@ from main_day_1_2 import calculate_similarity_score
 def test_calculate_similarity_score(tmp_path):
     input_file = tmp_path / "input.txt"
     input_data = """3   4
-4   3
-2   5
-1   3
-3   9
-3   3"""
+                    4   3
+                    2   5
+                    1   3
+                    3   9
+                    3   3"""
     input_file.write_text(input_data)
 
     result = calculate_similarity_score(str(input_file))
