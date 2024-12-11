3d36cf54ad1a391f927b8fd87bacb506a9f6d0f9
Laurent Thiebault <thiebault.laurent@gmail.com>
2024-12-11 23:02:57 +0100

    refactor: tests for day_11

--- day_11/test_day_11_2.py
+++ day_11/test_day_11_2.py
@@ -1,11 +1,11 @@
-from main_day_11_2 import calculate_number_of_stones
+from main_day_11_2 import calculate_number_of_stones_optimized
 
 
-def test_calculate_number_of_stones(tmp_path):
+def test_calculate_number_of_stones_optimized(tmp_path):
     input_file = tmp_path / "input.txt"
     input_data = "125 17"
     input_file.write_text(input_data)
 
-    result = calculate_number_of_stones(str(input_file))
+    result = calculate_number_of_stones_optimized(str(input_file))
     print(result)
-    assert result == 55312
+    assert result == 65601038650482
