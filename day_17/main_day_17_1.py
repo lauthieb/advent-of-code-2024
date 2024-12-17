62e3323c34f77e8576987e5ef9a5b62ce1ebcc28
Laurent Thiebault <thiebault.laurent@gmail.com>
2024-12-17 09:21:52 +0100

    feat: add day_17 (part 1)

---
+++ day_17/main_day_17_1.py
@@ -0,0 +1,112 @@
+import sys
+import os
+
+try:
+    from utils import measure_time
+except ImportError:
+    project_root = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), ".."))
+    sys.path.insert(0, project_root)
+    from utils import measure_time
+
+CURRENT_DIR = os.path.dirname(__file__)
+INPUT_FILE = os.path.join(CURRENT_DIR, "input.txt")
+
+
+def get_operand_value(operand, registers, is_combo=False):
+    if not is_combo:
+        return operand
+    if operand in [0, 1, 2, 3]:
+        return operand
+    if operand == 4:
+        return registers["A"]
+    if operand == 5:
+        return registers["B"]
+    if operand == 6:
+        return registers["C"]
+    raise ValueError(f"Not valid operand: {operand}")
+
+
+def execute_program(program, registers):
+    output = []
+    instruction_pointer = 0
+
+    while instruction_pointer < len(program):
+        opcode = program[instruction_pointer]
+        operand = program[instruction_pointer + 1]
+
+        if opcode == 0:  # adv: Divide A by 2^combo_operand
+            combo_value = get_operand_value(operand, registers, is_combo=True)
+            registers["A"] //= 2 ** combo_value
+
+        elif opcode == 1:  # bxl: XOR bit to bit between B and literal_operand
+            literal_value = get_operand_value(
+                operand, registers, is_combo=False)
+            registers["B"] ^= literal_value
+
+        elif opcode == 2:  # bst: B takes the value combo_operand % 8
+            combo_value = get_operand_value(
+                operand, registers, is_combo=True)
+            registers["B"] = combo_value % 8
+
+        elif opcode == 3:  # jnz: Jump if A != 0
+            literal_value = get_operand_value(
+                operand, registers, is_combo=False)
+            if registers["A"] != 0:
+                instruction_pointer = literal_value
+                continue  # Don't increment the pointer
+
+        elif opcode == 4:  # bxc: XOR bit to bit between B and C
+            registers["B"] ^= registers["C"]
+
+        elif opcode == 5:  # out: Output combo_operand % 8
+            combo_value = get_operand_value(operand, registers, is_combo=True)
+            output.append(combo_value % 8)
+
+        elif opcode == 6:  # bdv: Division like adv but result inside B
+            combo_value = get_operand_value(operand, registers, is_combo=True)
+            registers["B"] = registers["A"] // (2 ** combo_value)
+
+        elif opcode == 7:  # cdv: Division like adv but result inside C
+            combo_value = get_operand_value(operand, registers, is_combo=True)
+            registers["C"] = registers["A"] // (2 ** combo_value)
+
+        else:
+            raise ValueError(f"Opcode not supported: {opcode}")
+
+        instruction_pointer += 2
+
+    return output
+
+
+@measure_time
+def run_chronospatial_computer(input_file):
+    with open(input_file, "r") as f:
+        lines = f.readlines()
+
+    registers = {"A": 0, "B": 0, "C": 0}
+    program = []
+
+    for line in lines:
+        line = line.lstrip()
+        if line.startswith("Register A"):
+            registers["A"] = int(line.split(":")[1].strip())
+        elif line.startswith("Register B"):
+            registers["B"] = int(line.split(":")[1].strip())
+        elif line.startswith("Register C"):
+            registers["C"] = int(line.split(":")[1].strip())
+        elif line.startswith("Program"):
+            program = list(map(int, line.split(":")[1].strip().split(",")))
+
+    output = execute_program(program, registers)
+    return ",".join(map(str, output))
+
+
+if __name__ == "__main__":
+    try:
+        result = run_chronospatial_computer(INPUT_FILE)
+        print(result)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
+    exit(0)
---
+++ day_17/test_day_17_1.py
@@ -0,0 +1,15 @@
+from main_day_17_1 import run_chronospatial_computer
+
+
+def test_run_chronospatial_computer(tmp_path):
+    input_file = tmp_path / "input.txt"
+    input_data = """Register A: 729
+                    Register B: 0
+                    Register C: 0
+
+                    Program: 0,1,5,4,3,0"""
+    input_file.write_text(input_data)
+
+    result = run_chronospatial_computer(str(input_file))
+    print('result', result)
+    assert result == "4,6,3,5,6,3,5,2,1,0"
